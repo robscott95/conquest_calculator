b8568ec539352927381be0d681ba9e57e2c2c806
Bobert <robertkuska95@gmail.com>
2024-03-23 13:11:12 +0100

    Fixed order

--- active_unit/summary_block.py
+++ active_unit/summary_block.py
@@ -7,14 +7,14 @@ def show(target, no_of_die, no_of_stands_engaged, no_of_stands_all):
     st.write("Attack/Barrage: ", no_of_die)
     st.write("Engaged stands: ", no_of_stands_engaged)
     st.write("Supporting stands: ", no_of_stands_all - no_of_stands_engaged)
-    modified_abilities = [key for key, value in st.session_state.special_abilities_values.items() if value.get("is_modified", False)]
+    
+    container = st.container()  # Using container to group pills
+    container.write("Special abilities: ")
 
-    if modified_abilities:
-        st.markdown("Special abilities: ", unsafe_allow_html=True)
-        for ability in modified_abilities:
-            # Display each modified ability as a pill label
-            ability_label = st.session_state.special_abilities_values[ability].get("label", ability)  # Fallback to the key if label not present
-            # ability_label = ability_label.
-            st.markdown(generate_pill_label(ability_label), unsafe_allow_html=True)
-    else:
-        st.write("No special effects modified.")
+    for ability, details in st.session_state.special_abilities_values.items():
+        if details['is_modified']:
+            # Replace (X) or (+X) with the actual value
+            formatted_label = details['label'].replace('(X)', f'({details["value"]})').replace('(+X)', f'(+{details["value"]})')
+            
+            # Display each modified ability as a pi
+            container.markdown(generate_pill_label(formatted_label), unsafe_allow_html=True)
