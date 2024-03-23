359f24a1c164d47d51f0ce6c26fbd40522eb0caa
Bobert <robertkuska95@gmail.com>
2024-03-23 11:35:13 +0100

    removed unnecessary code

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from active_unit.active_unit import show as show_active_unit
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-def update_special_ability(key, widget_func):
-    st.session_state.special_abilities_values[key] = widget_func()
-
 # Title of the app
 st.title('Conquest Wound Calculator')
 
