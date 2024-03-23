d64bcf5e8459c90eee9a2306e188a49d3e48540b
Bobert <robertkuska95@gmail.com>
2024-03-23 15:09:02 +0100

    Added expected number of hits

--- results/stats_util.py
+++ results/stats_util.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 def calc_expected_hits(number_of_attacks, target, special_abilities):
-    expected_hits = number_of_attacks * (target/6)
+    p = (target/6)
+    expected_hits = number_of_attacks * p
     return expected_hits
+
+# uzyj clopper pearsona do estymacji granic/confidence intervalu
+# poniewaz normalne binomialne metody moga sie nie sprawdzic ze wzgledu na maly sample size.
